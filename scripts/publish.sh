7fbd775bd9aaa658bd08ca5c328e237a72b9a7a7
Simon Bertrand <84455908+Simon-Bertrand@users.noreply.github.com>
2023-04-29 23:39:16 +0200

    trying to fix the version issue inside workflows

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 rm -rf build
 rm -rf dist
+echo "github ref ${GITHUB_REF##*/}" #DEBUG
 python setup.py version
 python setup.py sdist bdist_wheel
 twine upload --skip-existing -r testpypi dist/* -u "Simon-Bertrand" -p $1
