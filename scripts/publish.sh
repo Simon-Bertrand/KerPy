4dad468fa65f0d1602f0db39396287266cd2b2e3
Simon Bertrand <84455908+Simon-Bertrand@users.noreply.github.com>
2023-04-29 23:47:45 +0200

    trying to fix the version issue inside workflows

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm -rf build
 rm -rf dist
-git fetch -–all -–tags
+git fetch --all --tags
 python setup.py version
 python setup.py sdist bdist_wheel
 twine upload --skip-existing -r testpypi dist/* -u "Simon-Bertrand" -p $1
