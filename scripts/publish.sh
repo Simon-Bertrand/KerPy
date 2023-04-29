67bbaf321943cb7a7da1b289234259a3d1fafc9c
Simon Bertrand <84455908+Simon-Bertrand@users.noreply.github.com>
2023-04-30 00:13:20 +0200

    changed workflow

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 rm -rf build
 rm -rf dist
-git fetch --all --tags
 python setup.py version
 python setup.py sdist bdist_wheel
 twine upload --skip-existing -r testpypi dist/* -u "Simon-Bertrand" -p $1
